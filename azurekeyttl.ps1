796f1e8f76761ed8a4b1506671af1a1e435a7a31
Andrey Petrov <andrey.petrov@dukascopy.com>
2022-05-12 18:20:58 +0200

    First Commit

---
+++ azurekeyttl.ps1
@@ -0,0 +1,84 @@
+#!/usr/bin/pwsh
+
+$settings = $PSScriptRoot + "/settings.json"
+
+$SettingsObject = Get-Content -Path $settings | ConvertFrom-Json
+
+$appid = $SettingsObject.appid
+$tenantid = $SettingsObject.tenantid
+$secret = $SettingsObject.secret
+
+$lldObject = @()
+$body =  @{                                                                                                                                
+     Grant_Type    = "client_credentials"                                
+     Scope         = "https://graph.microsoft.com/.default"
+     Client_Id     = $appid                                     
+     Client_Secret = $secret
+}
+
+$requestType=$args[1]
+$keyType = $args[2]
+$aadAppObjId = $args[3]
+$KeyId = $args[4]
+
+function ConnectAzure {
+$connection = Invoke-RestMethod -Uri https://login.microsoftonline.com/$tenantid/oauth2/v2.0/token -Method POST -Body $body
+$token = $connection.access_token
+
+$result = Connect-MgGraph -AccessToken $token
+if ($result -notmatch "Welcome To Microsoft Graph") {
+		Exit
+	}
+}
+
+function Get-ScriptDirectory {
+    Split-Path -Parent $PSCommandPath
+}
+
+function appLld {
+
+ConnectAzure
+$Apps = Get-MgApplication -All
+
+$Apps | %{
+    $aadAppObjId = $_.Id
+    $app = Get-MgApplication -ApplicationId $aadAppObjId 
+    $app.KeyCredentials | %{
+        $lldObject += [PSCustomObject] @{
+            "{#CredentialType}" = "KeyCredentials";
+            "{#DisplayName}" = $app.DisplayName;
+            "{#AppId}" = $app.AppId;
+            "{#KeyID}" = $_.KeyId;
+	    "{#aadAppObjId}" = $aadAppObjId;
+            }
+    }
+
+    $app.PasswordCredentials | %{
+        $lldObject += [PSCustomObject] @{
+            "{#CredentialType}" = "PasswordCredentials";
+            "{#DisplayName}" = $app.DisplayName;
+            "{#AppId}" = $app.AppId;
+            "{#KeyID}" = $_.KeyId;
+            "{#aadAppObjId}" = $aadAppObjId;
+        }
+    }
+}
+
+$lldObject | ConvertTo-Json	
+}
+
+function AppKeyTtl {
+	ConnectAzure
+	$today = Get-Date
+	$app = Get-MgApplication -ApplicationId $aadAppObjId
+	$appKey = $app.PasswordCredentials | Where-Object {$_.KeyId -eq $KeyId}
+	$Expired = $appKey.EndDateTime - $today
+	$Expired.Days	
+	
+}
+
+switch ($requestType) {
+	'lld'  { appLld }
+	'keyTtl' { AppKeyTtl($keyType, $aadAppObjId, $KeyId) }	
+}
+
